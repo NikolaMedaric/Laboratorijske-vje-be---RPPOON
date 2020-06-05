70be833201c3bcc18ed08b6ac2241bb4a29ac244
NikolaMedaric <63855981+NikolaMedaric@users.noreply.github.com>
2020-06-05 17:24:03 +0200

    Add files via upload

---
+++ Note.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lv1
+{
+    class Note
+    {
+        private String text;
+        private String author;
+        private int importance;
+
+        public string Text
+        {
+            get { return this.text; }
+            set { this.text = value; }
+        }
+
+        public string Author
+        {
+            get { return this.author; }
+            private set { this.author = value; }
+        }
+
+        public int Priority
+        {
+            get { return this.importance; }
+            set { this.importance = value; }
+        }
+
+        public String getText()
+        {
+            return this.text;
+        }
+
+        public String getAuthor()
+        {
+            return this.author;
+        }
+
+        public int getImportance()
+        {
+            return this.importance;
+        }
+
+        public void setText(String text)
+        {
+            this.text = text;
+        }
+
+        public void setPriority(int importance)
+        {
+            this.importance = importance;
+        }
+
+        public Note()
+        {
+            this.text = "LV1-RPPOON";
+            this.author = "Nikola Medarić";
+            this.importance = 3;
+        }
+
+        public Note(string text)
+        {
+            this.text = text;
+            this.author = "Nikola Medarić";
+            this.importance = 3;
+        }
+
+        public Note(string text, int importance)
+        {
+            this.text = text;
+            this.author = "Nikola Medarić";
+            this.importance = importance;
+        }
+
+        public override string ToString()
+        {
+            return this.text + this.author + this.importance;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Note note1, note2, note3;
+            note1 = new Note();
+            note2 = new Note("New Text");
+            note3 = new Note("Changing Importance", 2);
+            Console.WriteLine(note1.getAuthor());
+            Console.WriteLine(note1.getText());
+            Console.WriteLine(note2.getAuthor());
+            Console.WriteLine(note2.getText());
+            Console.WriteLine(note3.getText());
+            Console.WriteLine(note3.getAuthor());
+           
+        }
+    }
+}
